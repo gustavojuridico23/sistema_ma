5d674b52e0786f021144b7ee50b5a7185a8bf586
Gustavo <gustavojuridico23@gmail.com>
2020-07-15 19:52:48 -0300

    Updates

---
+++ precache-manifest.15283522dad90ab64d503a7d2f779bb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f083063e937a753ed0e9adbd97e977",
+    "url": "/sistema_ma/index.html"
+  },
+  {
+    "revision": "03b188b382d11ed7c60b",
+    "url": "/sistema_ma/static/css/main.9a54fb5f.chunk.css"
+  },
+  {
+    "revision": "24487c787d0c011bc9f1",
+    "url": "/sistema_ma/static/js/2.d22dc682.chunk.js"
+  },
+  {
+    "revision": "62242b61015eca77983672faac11aabc",
+    "url": "/sistema_ma/static/js/2.d22dc682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03b188b382d11ed7c60b",
+    "url": "/sistema_ma/static/js/main.13da38ea.chunk.js"
+  },
+  {
+    "revision": "d90189625da868675a3a",
+    "url": "/sistema_ma/static/js/runtime-main.7c6a8661.js"
+  }
+]);
